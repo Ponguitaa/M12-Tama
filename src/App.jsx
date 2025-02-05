22ded026ec2af92aeb0fb8e74eb4fcd06e58a025
Ponguitaa <sergiponga18@gmail.com>
2025-02-05 17:35:27 +0000

    Commit desde Raspberry

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { Tamagotchi } from './components/Tamagotchi';
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen flex justify-center items-center">
+    <div className="bg-gray-150 min-h-screen flex justify-center items-center">
       <Tamagotchi />
+    
     </div>
   );
 }
